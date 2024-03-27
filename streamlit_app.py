dcf80a98ff30917e5220ba0a4e6bac17baa65d3f
DoNalD-A <162666567+DoNalD-A@users.noreply.github.com>
2024-03-27 02:05:20 +0000

    second commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,14 @@ import altair as alt
 # Page title
 st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
 st.title('📊 Interactive Data Explorer')
+
+st.title("this is the app title")
+st.header("this is the header")
+st.markdown("this is the markdown")
+st.subheader("this isthe subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+
+
+st.checkbox('yes')
+st.butoon("Click")
